a793b93f961ac137584a8e006e567b2723f28914
Ângelo Nuffer <angelonuffer@gmail.com>
2015-05-26 22:32:18 +0000

    Give username as command line argument.

--- orgdoc/__init__.py
+++ orgdoc/__init__.py
@@ -39,21 +39,26 @@ def verify_dir(directory, relative_path):
         if os.path.isdir(path):
             verify_dir(directory, os.path.sep.join([relative_path, child]))
 
+def get_default_username():
+    return os.getlogin()
+
 def sign():
     arguments_parser = ArgumentParser(description="Sign organization documents.")
     arguments_parser.add_argument("path", help="path to document")
+    arguments_parser.add_argument("-u", "--username", default=get_default_username(), help="username of signer")
     arguments = arguments_parser.parse_args()
     path = vars(arguments)["path"]
+    username = vars(arguments)["username"]
     directory = get_directory(path)
-    privkey_path = os.path.sep.join(directory + ["pubkeys", "%s.privkey.pem" % os.getlogin()])
+    privkey_path = os.path.sep.join(directory + ["pubkeys", "%s.privkey.pem" % username])
     if not os.path.isfile(privkey_path):
-        pubkey_path = os.path.sep.join(directory + ["pubkeys", "%s.pubkey.pem" % os.getlogin()])
+        pubkey_path = os.path.sep.join(directory + ["pubkeys", "%s.pubkey.pem" % username])
         subprocess.call(["openssl", "genpkey", "-algorithm", "RSA", "-pkeyopt", "rsa_keygen_bits:2048", "-pkeyopt", "rsa_keygen_pubexp:3", "-out", privkey_path])
         os.popen2(["openssl", "pkey", "-in", privkey_path, "-out", pubkey_path, "-pubout"])
     relative_path = os.path.relpath(path, start=os.path.sep.join(directory))
     signature_path = os.path.sep.join(directory + ["signatures", relative_path])
     os.popen2(["mkdir", "-p", os.path.dirname(signature_path)])
-    os.popen2(["openssl", "dgst", "-sha1", "-sign", privkey_path, "-out", "%s.%s.bin" % (signature_path, os.getlogin()), path])
+    os.popen2(["openssl", "dgst", "-sha1", "-sign", privkey_path, "-out", "%s.%s.bin" % (signature_path, username), path])
 
 def verify():
     directory = get_directory(".")
